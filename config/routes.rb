fd733b10e43b004cde5dcca1bfd74cbbab861286
vorellana <orellana.castro.victor@gmail.com>
2016-11-21 19:34:27 -0300

    Second commit Devise add name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :genres
   root 'genres#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
