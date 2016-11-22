4c573eef49a1fce7c0764a42f06b2012f188e37c
vorellana <orellana.castro.victor@gmail.com>
2016-11-21 22:03:52 -0300

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   
   resources :genres
+  resources :songs
   root 'songs#index'
   # root 'genres#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
